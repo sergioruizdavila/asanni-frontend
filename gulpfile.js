791b6a83c5236a27ed6d0756d53bf6bedff4caea
sergioruizdavila <sergioruizdavila@gmail.com>
2016-08-26 21:10:13 -0500

    Mod: Refactor on gulp.file.js and gitignore

---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var connect = require('gulp-connect');
+
+/*Path Files*/
+var paths = {
+    htmlTemplates: 'www/app/**/*.html',
+    appTypescript: ['www/**/*.ts', '!typings/**/*.*'],
+    appJs: ['www/**/*.js', 'www/**/*.js.map'],
+    appSass: ['www/**/**/*.scss'],
+    inputSass: 'www/app/app.scss',
+    outputSass: 'www/app',
+    sassdocOptions: {dest: './www/css/doc'}
+};
+
+
+/**
+ * LOCAL SERVER
+ * @desc This task is the responsible to run a local server in order to work locally
+ */
+
+gulp.task('webserver', function() {
+  connect.server({
+    root: 'www',
+    livereload: true
+  });
+});
+
+/**
+ * SASS to CSS - based on http://www.sitepoint.com/simple-gulpy-workflow-sass/
+ * @desc This task take app.scss and transform this to .css, after that put each new .css into App_Web -> dist -> styles
+ */
+
+var sassOptions = {
+    errLogToConsole: true
+};
+
+gulp.task('sass', function() {
+  gulp
+    .src(paths.inputSass)
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.outputSass))
+    .pipe(connect.reload());
+});
+
+/**
+ * HTML RELOAD
+ * @desc This task is the responsible to reload browser when one html has changed
+ */
+
+gulp.task('html', function () {
+  gulp.src(paths.htmlTemplates)
+    .pipe(connect.reload());
+});
+
+/**
+ * TS RELOAD
+ * @desc This task is the responsible to reload browser when one .ts has changed
+ */
+
+gulp.task('ts', function () {
+  gulp.src(paths.appTypescript)
+    .pipe(connect.reload());
+});
+
+/**
+ * WATCH METHOD
+ * @desc This task is the responsible to listen each change on some files in order to reload browser or
+ * doing some special task
+ */
+
+gulp.task('watch', function() {
+    gulp.watch(paths.inputSass, ['sass']);
+    gulp.watch([paths.htmlTemplates], ['html']);
+    gulp.watch([paths.appTypescript], ['ts']);
+})
+
+
+gulp.task('default', ['sass', 'webserver', 'watch']);
