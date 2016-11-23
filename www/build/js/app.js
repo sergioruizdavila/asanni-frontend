2045c2b23e2ca41ddc4ccb691570f639e30f014c
sergioruizdavila <sergioruizdavila@gmail.com>
2016-11-23 15:10:11 -0500

    Mod: Prepare to deploy on Staging

--- www/build/js/app.js
+++ www/build/js/app.js
@@ -81,7 +81,7 @@
 (function () {
     'use strict';
     var dataConfig = {
-        baseUrl: 'http://localhost:3000/',
+        baseUrl: 'https://asanni.herokuapp.com/api/v1/',
         googleMapKey: 'AIzaSyD-vO1--MMK-XmQurzNQrxW4zauddCJh5Y',
         mixpanelToken: '86a48c88274599c662ad64edb74b12da',
         modalMeetingPointTmpl: 'components/modal/modalMeetingPoint/modalMeetingPoint.html',
