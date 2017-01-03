302b7cb4ae10d572c6b598cf7b4abde5aba694a4
sergioruizdavila <sergioruizdavila@gmail.com>
2017-01-02 18:16:53 -0800

    Add: Created a new button style: button with icon and without text Add: Created new landing page html Mod: Refactor on footer in order to add facebook and twitter button, and remove plenty of information

--- www/build/js/app.js
+++ www/build/js/app.js
@@ -3502,6 +3502,141 @@ var app;
 (function () {
     'use strict';
     angular
+        .module('mainApp.pages.landingPage', [])
+        .config(config);
+    function config($stateProvider) {
+        $stateProvider
+            .state('page.landingPage', {
+            url: '/main',
+            views: {
+                'container': {
+                    templateUrl: 'app/pages/landingPage/landingPage.html',
+                    controller: 'mainApp.pages.landingPage.LandingPageController',
+                    controllerAs: 'vm'
+                }
+            },
+            parent: 'page',
+            onEnter: ['$rootScope', function ($rootScope) {
+                    $rootScope.activeHeader = false;
+                    $rootScope.activeFooter = true;
+                }]
+        });
+    }
+})();
+//# sourceMappingURL=landingPage.config.js.map
+var app;
+(function (app) {
+    var pages;
+    (function (pages) {
+        var landingPage;
+        (function (landingPage) {
+            var LandingPageController = (function () {
+                function LandingPageController($state, $translate, LandingPageService) {
+                    this.$state = $state;
+                    this.$translate = $translate;
+                    this.LandingPageService = LandingPageService;
+                    this._init();
+                }
+                LandingPageController.prototype._init = function () {
+                    this.form = {
+                        userData: {
+                            name: '',
+                            email: '',
+                            comment: ''
+                        },
+                        language: this.$translate.use() || 'en'
+                    };
+                    this.success = false;
+                    this.sending = false;
+                    this.activate();
+                };
+                LandingPageController.prototype.activate = function () {
+                    var self = this;
+                    console.log('landingPage controller actived');
+                };
+                LandingPageController.prototype.changeLanguage = function () {
+                    this.$translate.use(this.form.language);
+                };
+                LandingPageController.prototype.goToEarlyAccessForm = function () {
+                    document.querySelector('.landingPage__early-access-block').scrollIntoView({ behavior: 'smooth' });
+                };
+                LandingPageController.prototype.goDown = function () {
+                    document.querySelector('.landingPage__title-block').scrollIntoView({ behavior: 'smooth' });
+                };
+                LandingPageController.prototype.createEarlyAdopter = function () {
+                    var self = this;
+                    this.sending = true;
+                    mixpanel.track("Click on Notify button", {
+                        "name": this.form.userData.name || '*',
+                        "email": this.form.userData.email,
+                        "comment": this.form.userData.comment || '*'
+                    });
+                    var userData = {
+                        name: this.form.userData.name || '*',
+                        email: this.form.userData.email,
+                        comment: this.form.userData.comment || '*'
+                    };
+                    this.LandingPageService.createEarlyAdopter(userData).then(function (response) {
+                        if (response.createdAt) {
+                            self.success = true;
+                        }
+                        else {
+                            self.sending = false;
+                        }
+                    });
+                };
+                return LandingPageController;
+            }());
+            LandingPageController.controllerId = 'mainApp.pages.landingPage.LandingPageController';
+            LandingPageController.$inject = ['$state',
+                '$translate',
+                'mainApp.pages.landingPage.LandingPageService'];
+            landingPage.LandingPageController = LandingPageController;
+            angular
+                .module('mainApp.pages.landingPage')
+                .controller(LandingPageController.controllerId, LandingPageController);
+        })(landingPage = pages.landingPage || (pages.landingPage = {}));
+    })(pages = app.pages || (app.pages = {}));
+})(app || (app = {}));
+//# sourceMappingURL=landingPage.controller.js.map
+var app;
+(function (app) {
+    var pages;
+    (function (pages) {
+        var landingPage;
+        (function (landingPage) {
+            'use strict';
+            var LandingPageService = (function () {
+                function LandingPageService(restApi) {
+                    this.restApi = restApi;
+                }
+                LandingPageService.prototype.createEarlyAdopter = function (userData) {
+                    var url = 'early/';
+                    return this.restApi.create({ url: url }, userData).$promise
+                        .then(function (data) {
+                        return data;
+                    }).catch(function (err) {
+                        console.log(err);
+                        return err;
+                    });
+                };
+                return LandingPageService;
+            }());
+            LandingPageService.serviceId = 'mainApp.pages.landingPage.LandingPageService';
+            LandingPageService.$inject = [
+                'mainApp.core.restApi.restApiService'
+            ];
+            landingPage.LandingPageService = LandingPageService;
+            angular
+                .module('mainApp.pages.landingPage')
+                .service(LandingPageService.serviceId, LandingPageService);
+        })(landingPage = pages.landingPage || (pages.landingPage = {}));
+    })(pages = app.pages || (app.pages = {}));
+})(app || (app = {}));
+//# sourceMappingURL=landingPage.service.js.map
+(function () {
+    'use strict';
+    angular
         .module('mainApp.pages.signUpPage', [])
         .config(config);
     function config($stateProvider) {
