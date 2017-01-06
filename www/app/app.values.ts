80e66e6a615fc465596bd252a866ffe1f06efd92
sergioruizdavila <sergioruizdavila@gmail.com>
2017-01-06 13:18:34 -0800

    Mod: Clean styles on htmls Add: Created a new component style: quotes

--- www/app/app.values.ts
+++ www/app/app.values.ts
@@ -35,7 +35,7 @@ interface IDataConfig {
     // Use: command - json-server --watch db.json
     // DEV Local
     // baseUrl: 'http://127.0.0.1:8000/api/v1/'
-    // bucketS3: 'waysily-img'
+    // bucketS3: 'waysily-img/teachers-avatar-dev'
     // DEV Heroku
     // baseUrl: 'https://waysily-server-dev.herokuapp.com/api/v1/'
     // bucketS3: 'waysily-img/teachers-avatar-dev'
@@ -44,7 +44,7 @@ interface IDataConfig {
     // bucketS3: 'waysily-img/teachers-avatar-prd'
     var dataConfig: IDataConfig = {
         currentYear: '2017',
-        baseUrl: 'https://waysily-server.herokuapp.com/api/v1/',
+        baseUrl: 'http://127.0.0.1:8000/api/v1/',
         googleMapKey: 'AIzaSyD-vO1--MMK-XmQurzNQrxW4zauddCJh5Y',
         mixpanelToken: '86a48c88274599c662ad64edb74b12da',
         modalMeetingPointTmpl: 'components/modal/modalMeetingPoint/modalMeetingPoint.html',
@@ -53,7 +53,7 @@ interface IDataConfig {
         modalEducationTmpl: 'components/modal/modalEducation/modalEducation.html',
         modalCertificateTmpl: 'components/modal/modalCertificate/modalCertificate.html',
         modalSignUpTmpl: 'components/modal/modalSignUp/modalSignUp.html',
-        bucketS3: 'waysily-img/teachers-avatar-prd',
+        bucketS3: 'waysily-img/teachers-avatar-dev',
         regionS3: 'us-east-1',
         accessKeyIdS3: 'AKIAIHKBYIUQD4KBIRLQ',
         secretAccessKeyS3: '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',
