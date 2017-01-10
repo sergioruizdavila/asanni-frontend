91c46323bfc230b8e98f446a083c7a7b02d98897
sergioruizdavila <sergioruizdavila@gmail.com>
2017-01-09 20:57:10 -0800

    Mod: Prepare code to deploy to DEV environment

--- www/build/js/app.js
+++ www/build/js/app.js
@@ -63,7 +63,7 @@
     'use strict';
     var dataConfig = {
         currentYear: '2017',
-        baseUrl: 'http://127.0.0.1:8000/api/v1/',
+        baseUrl: 'https://waysily-server.herokuapp.com/api/v1/',
         googleMapKey: 'AIzaSyD-vO1--MMK-XmQurzNQrxW4zauddCJh5Y',
         mixpanelToken: '86a48c88274599c662ad64edb74b12da',
         modalMeetingPointTmpl: 'components/modal/modalMeetingPoint/modalMeetingPoint.html',
@@ -72,7 +72,7 @@
         modalEducationTmpl: 'components/modal/modalEducation/modalEducation.html',
         modalCertificateTmpl: 'components/modal/modalCertificate/modalCertificate.html',
         modalSignUpTmpl: 'components/modal/modalSignUp/modalSignUp.html',
-        bucketS3: 'waysily-img/teachers-avatar-dev',
+        bucketS3: 'waysily-img/teachers-avatar-prd',
         regionS3: 'us-east-1',
         accessKeyIdS3: 'AKIAIHKBYIUQD4KBIRLQ',
         secretAccessKeyS3: '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',
