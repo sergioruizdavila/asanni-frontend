780e41c525421dcd2b4a2700917f62e76d273170
sergioruizdavila <sergioruizdavila@gmail.com>
2017-01-22 14:50:27 -0800

    Mod: Prepare code to deploy to PRD environment

--- www/app/app.values.ts
+++ www/app/app.values.ts
@@ -45,7 +45,7 @@ interface IDataConfig {
     // bucketS3: 'waysily-img/teachers-avatar-prd'
     var dataConfig: IDataConfig = {
         currentYear: '2017',
-        baseUrl: 'https://waysily-server-dev.herokuapp.com/api/v1/',
+        baseUrl: 'https://waysily-server.herokuapp.com/api/v1/',
         googleMapKey: 'AIzaSyD-vO1--MMK-XmQurzNQrxW4zauddCJh5Y',
         mixpanelToken: '86a48c88274599c662ad64edb74b12da',
         modalMeetingPointTmpl: 'components/modal/modalMeetingPoint/modalMeetingPoint.html',
@@ -54,7 +54,7 @@ interface IDataConfig {
         modalEducationTmpl: 'components/modal/modalEducation/modalEducation.html',
         modalCertificateTmpl: 'components/modal/modalCertificate/modalCertificate.html',
         modalSignUpTmpl: 'components/modal/modalSignUp/modalSignUp.html',
-        bucketS3: 'waysily-img/teachers-avatar-dev',
+        bucketS3: 'waysily-img/teachers-avatar-prd',
         regionS3: 'us-east-1',
         accessKeyIdS3: 'AKIAIHKBYIUQD4KBIRLQ',
         secretAccessKeyS3: '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',
