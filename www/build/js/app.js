6a3196329eafc87f2637b93c7d4e904d8bfd6528
sergioruizdavila <sergioruizdavila@gmail.com>
2017-01-24 18:51:20 -0800

    Add: Create first version of modalRecommendTeacher component (controller, html, style)

--- www/build/js/app.js
+++ www/build/js/app.js
@@ -3827,6 +3827,46 @@ var components;
     })(modal = components.modal || (components.modal = {}));
 })(components || (components = {}));
 //# sourceMappingURL=modalSignUp.controller.js.map
+var components;
+(function (components) {
+    var modal;
+    (function (modal) {
+        var modalRecommendTeacher;
+        (function (modalRecommendTeacher) {
+            var ModalRecommendTeacherController = (function () {
+                function ModalRecommendTeacherController($uibModalInstance, $timeout, $filter) {
+                    this.$uibModalInstance = $uibModalInstance;
+                    this.$timeout = $timeout;
+                    this.$filter = $filter;
+                    this._init();
+                }
+                ModalRecommendTeacherController.prototype._init = function () {
+                    var self = this;
+                    this.activate();
+                };
+                ModalRecommendTeacherController.prototype.activate = function () {
+                    console.log('modalRecommendTeacher controller actived');
+                };
+                ModalRecommendTeacherController.prototype.close = function () {
+                    this.$uibModalInstance.close();
+                };
+                return ModalRecommendTeacherController;
+            }());
+            ModalRecommendTeacherController.controllerId = 'mainApp.components.modal.ModalRecommendTeacherController';
+            ModalRecommendTeacherController.$inject = [
+                '$uibModalInstance',
+                'mainApp.core.util.GetDataStaticJsonService',
+                'mainApp.core.util.FunctionsUtilService',
+                'mainApp.models.teacher.TeacherService',
+                '$timeout',
+                '$filter'
+            ];
+            angular.module('mainApp.components.modal')
+                .controller(ModalRecommendTeacherController.controllerId, ModalRecommendTeacherController);
+        })(modalRecommendTeacher = modal.modalRecommendTeacher || (modal.modalRecommendTeacher = {}));
+    })(modal = components.modal || (components.modal = {}));
+})(components || (components = {}));
+//# sourceMappingURL=modalRecommendTeacher.controller.js.map
 (function () {
     'use strict';
     angular
