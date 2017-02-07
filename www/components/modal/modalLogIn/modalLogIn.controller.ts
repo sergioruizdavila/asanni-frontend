3f0e64fb4ee897404717682304c742c6ad82ea79
sergioruizdavila <sergioruizdavila@gmail.com>
2017-02-06 18:56:12 -0800

    Add: Added validation on Log In modal

--- www/components/modal/modalLogIn/modalLogIn.controller.ts
+++ www/components/modal/modalLogIn/modalLogIn.controller.ts
@@ -13,6 +13,7 @@ module components.modal.modalLogIn {
     /**********************************/
     interface IModalLogInController {
         close: () => void;
+        validate: IModalLogInValidate;
         activate: () => void;
     }
 
@@ -21,10 +22,16 @@ module components.modal.modalLogIn {
     }
 
     interface IModalLogInForm extends app.core.interfaces.IUserDataAuth {
+        username: string;
+        email: string;
+        password: string;
     }
 
     interface IModalLogInValidate {
+        username: app.core.util.functionsUtil.IValid;
         email: app.core.util.functionsUtil.IValid;
+        password: app.core.util.functionsUtil.IValid;
+        globalValidate: app.core.util.functionsUtil.IValid;
     }
 
 
@@ -46,6 +53,7 @@ module components.modal.modalLogIn {
             '$state',
             'mainApp.auth.AuthService',
             'mainApp.account.AccountService',
+            'mainApp.core.util.FunctionsUtilService',
             'mainApp.core.util.messageUtilService',
             'mainApp.localStorageService',
             'dataConfig',
@@ -62,6 +70,7 @@ module components.modal.modalLogIn {
             private $state: ng.ui.IStateService,
             private AuthService: app.auth.IAuthService,
             private AccountService: app.account.IAccountService,
+            private functionsUtil: app.core.util.functionsUtil.IFunctionsUtilService,
             private messageUtil: app.core.util.messageUtil.IMessageUtilService,
             private localStorage,
             private dataConfig: IDataConfig,
@@ -82,6 +91,14 @@ module components.modal.modalLogIn {
                 username: '',
                 email: '',
                 password: ''
+            };
+
+            // Build validate object fields
+            this.validate = {
+                username: {valid: true, message: ''},
+                email: {valid: true, message: ''},
+                password: {valid: true, message: ''},
+                globalValidate: {valid: true, message: ''}
             };
 
             this.activate();
@@ -108,60 +125,110 @@ module components.modal.modalLogIn {
             //VARIABLES
             let self = this;
 
-            //TODO: Validar bien esto, ya que toco hacer una rosca
-            // rara por que no encontre la forma de que OAuth2 permitiera
-            // loguear con email y no con username.
-            this.AccountService.getUsername(this.form.email).then(
-                function(response) {
+            let formValid = this._validateForm();
 
-                    //If username exists, add this field on request to login
-                    if(response.userExist) {
-                        self.form.username = response.username;
-                    } else {
-                        //TODO: Solucion nada optima, no esta bien asignar el email,
-                        // como si fuera el username, solo para que rompa, y devuelva
-                        // error de login, buscar una solucion mejor.
-                        self.form.username = self.form.email;
-                    }
+            if(formValid) {
 
-                    self.AuthService.login(self.form).then(
+                //TODO: Validar bien esto, ya que toco hacer una rosca
+                // rara por que no encontre la forma de que OAuth2 permitiera
+                // loguear con email y no con username.
+                this.AccountService.getUsername(this.form.email).then(
+                    function(response) {
 
-                        //Success
-                        function(response) {
-                            self.AccountService.getAccount().then(
-                                function(response) {
-                                    //LOG
-                                    DEBUG && console.log('Data User: ', response);
-
-                                    //Set logged User data in localStorage
-                                    self.localStorage.setItem('currentUser', JSON.stringify(response));
-                                    //Set logged User data in $rootScope
-                                    //TODO: Crear un modelo: Account, y crear un objeto
-                                    // tipo Account: new app.models.Account();
-                                    self.$rootScope.currentUser = JSON.parse(self.localStorage.getItem('currentUser'));
-                                    //Close modal
-                                    self.$uibModalInstance.close();
-                                }
-                            );
-                        },
-
-                        // Error
-                        function(response) {
-                            if (response.status == 401) {
-                                DEBUG && console.log('Incorrect username or password.');
-                            }
-
-                            else if (response.status == -1) {
-                                DEBUG && console.log('No response from server.');
-                            }
-
-                            else {
-                                DEBUG && console.log('There was a problem logging you in. Error code: ' + response.status + '.');
-                            }
+                        //If username exists, add this field on request to login
+                        if(response.userExist) {
+                            self.form.username = response.username;
+                        } else {
+                            //TODO: Solucion nada optima, no esta bien asignar el email,
+                            // como si fuera el username, solo para que rompa, y devuelva
+                            // error de login, buscar una solucion mejor.
+                            self.form.username = self.form.email;
                         }
-                    );
-                }
-            );
+
+                        self.AuthService.login(self.form).then(
+
+                            //Success
+                            function(response) {
+                                self.AccountService.getAccount().then(
+                                    function(response) {
+                                        //LOG
+                                        DEBUG && console.log('Data User: ', response);
+
+                                        //Set logged User data in localStorage
+                                        self.localStorage.setItem('currentUser', JSON.stringify(response));
+                                        //Set logged User data in $rootScope
+                                        //TODO: Crear un modelo: Account, y crear un objeto
+                                        // tipo Account: new app.models.Account();
+                                        self.$rootScope.currentUser = JSON.parse(self.localStorage.getItem('currentUser'));
+                                        //Close modal
+                                        self.$uibModalInstance.close();
+                                    }
+                                );
+                            },
+
+                            // Error
+                            function(response) {
+                                if (response.status == 401) {
+                                    //TODO: Traducir mensaje a español
+                                    DEBUG && console.log('Incorrect username or password.');
+                                    self.validate.globalValidate.valid = false;
+                                    self.validate.globalValidate.message = 'Incorrect username or password.';
+                                }
+
+                                else if (response.status == -1) {
+                                    //TODO: Traducir mensaje a español
+                                    DEBUG && console.log('No response from server. Try again, please');
+                                    self.messageUtil.error('No response from server. Try again, please');
+                                }
+
+                                else {
+                                    //TODO: Traducir mensaje a español
+                                    DEBUG && console.log('There was a problem logging you in. Error code: ' + response.status + '.');
+                                    self.messageUtil.error('There was a problem logging you in. Error code: ' + response.status + '.');
+                                }
+                            }
+                        );
+                    }
+                );
+
+            }
+
+        }
+
+
+
+        /**
+        * _validateForm
+        * @description - Validate each field on form
+        * @use - this._validateForm();
+        * @function
+        * @return {boolean} formValid - return If the complete form is valid or not.
+        */
+        private _validateForm(): boolean {
+            //CONSTANTS
+            const NULL_ENUM = app.core.util.functionsUtil.Validation.Null;
+            const EMPTY_ENUM = app.core.util.functionsUtil.Validation.Empty;
+            const EMAIL_ENUM = app.core.util.functionsUtil.Validation.Email;
+            /***************************************************/
+
+            //VARIABLES
+            let formValid = true;
+
+            //Validate Email field
+            let email_rules = [NULL_ENUM, EMPTY_ENUM, EMAIL_ENUM];
+            this.validate.email = this.functionsUtil.validator(this.form.email, email_rules);
+            if(!this.validate.email.valid) {
+                formValid = this.validate.email.valid;
+            }
+
+            //Validate Password field
+            let password_rules = [NULL_ENUM, EMPTY_ENUM];
+            this.validate.password = this.functionsUtil.validator(this.form.password, password_rules);
+            if(!this.validate.password.valid) {
+                formValid = this.validate.password.valid;
+            }
+
+            return formValid;
         }
 
 
