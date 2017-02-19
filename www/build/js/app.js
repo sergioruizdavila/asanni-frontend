5ee287b39600f4a96552e9a68d373dc05d64091d
sergioruizdavila <sergioruizdavila@gmail.com>
2017-02-18 17:24:20 -0800

    Add: Added new styles component: 'flags' Mod: Refactor change language functionality in order to show it on footer (not select list component)

--- www/build/js/app.js
+++ www/build/js/app.js
@@ -3455,18 +3455,37 @@ var components;
             .module('mainApp.components.footer')
             .directive(MaFooter.directiveId, MaFooter.instance);
         var FooterController = (function () {
-            function FooterController() {
+            function FooterController(functionsUtil) {
+                this.functionsUtil = functionsUtil;
                 this.init();
             }
             FooterController.prototype.init = function () {
+                var currentLanguageCode = this.functionsUtil.getCurrentLanguage() || 'en';
+                var languageLabel = '%header.lang.options.' + currentLanguageCode + '.text';
+                this.form = {
+                    language: {
+                        key: currentLanguageCode,
+                        value: languageLabel
+                    }
+                };
+                this.assignFlag = 'ma-flag--default--flag-' + this.form.language.key;
                 this.activate();
             };
             FooterController.prototype.activate = function () {
                 console.log('footer controller actived');
             };
+            FooterController.prototype.changeLanguage = function (code) {
+                this.functionsUtil.changeLanguage(code);
+                this.form.language.key = code;
+                this.form.language.value = '%header.lang.options.' + code + '.text';
+                this.assignFlag = 'ma-flag--default--flag-' + code;
+            };
             return FooterController;
         }());
         FooterController.controllerId = 'mainApp.components.footer.FooterController';
+        FooterController.$inject = [
+            'mainApp.core.util.FunctionsUtilService'
+        ];
         footer.FooterController = FooterController;
         angular.module('mainApp.components.footer')
             .controller(FooterController.controllerId, FooterController);
