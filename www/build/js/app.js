28b38242dc4a10a828c2ea3089a01d0da2088cda
sergioruizdavila <sergioruizdavila@gmail.com>
2017-03-02 17:15:07 -0800

    Mod: Prepare to deploy to DEV environment

--- www/build/js/app.js
+++ www/build/js/app.js
@@ -97,7 +97,7 @@ DEBUG = true;
     var BASE_URL = 'https://waysily-server.herokuapp.com/api/v1/';
     var BUCKETS3 = 'waysily-img/teachers-avatar-prd';
     if (DEBUG) {
-        BASE_URL = 'http://127.0.0.1:8000/api/v1/';
+        BASE_URL = 'https://waysily-server-dev.herokuapp.com/api/v1/';
         BUCKETS3 = 'waysily-img/teachers-avatar-dev';
     }
     var dataConfig = {
