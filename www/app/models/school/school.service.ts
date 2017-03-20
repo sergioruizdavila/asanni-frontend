fcb0ad483e90753d34ca4d040be7f02e19386b30
sergioruizdavila <sergioruizdavila@gmail.com>
2017-03-19 23:05:24 -0700

    Add: Created School Services

--- www/app/models/school/school.service.ts
+++ www/app/models/school/school.service.ts
@@ -14,7 +14,15 @@ module app.models.school {
     /**********************************/
     export interface ISchoolService {
         getSchoolById: (id: string) => angular.IPromise<any>;
+        getSchoolByUserId: (userId: string) => angular.IPromise<any>;
         getAllSchools: () => angular.IPromise<any>;
+    }
+
+    export interface ISchoolQueryObject {
+        next: string;
+        previous: string;
+        count: number;
+        results: Array<app.models.school.School>;
     }
 
 
@@ -28,22 +36,34 @@ module app.models.school {
         /**********************************/
         /*           PROPERTIES           */
         /**********************************/
-
+        SCHOOL_URI: string;
+        USER_SCHOOL_URI: string;
         // --------------------------------
 
 
         /*-- INJECT DEPENDENCIES --*/
         static $inject = [
-            'mainApp.core.restApi.restApiService'
+            'mainApp.core.restApi.restApiService',
+            'mainApp.auth.AuthService',
+            '$q'
         ];
 
 
         /**********************************/
         /*           CONSTRUCTOR          */
         /**********************************/
-        constructor(private restApi: app.core.restApi.IRestApi) {
+        constructor(
+            private restApi: app.core.restApi.IRestApi,
+            private AuthService: app.auth.IAuthService,
+            private $q: angular.IQService) {
+
             //LOG
-            console.log('school service instanced');
+            DEBUG && console.log('schools service instanced');
+
+            //CONSTANTS
+            this.SCHOOL_URI = 'schools';
+            this.USER_SCHOOL_URI = 'schools?userId=';
+
         }
 
         /**********************************/
@@ -55,25 +75,70 @@ module app.models.school {
         * @description - get school by Id
         * @use - this.SchoolService.getSchoolById('98d667ae');
         * @function
-        * @params {string} id - school id
+        * @params {string} id - user id
         * @return {angular.IPromise<any>} promise - return school by Id
         */
         getSchoolById(id): angular.IPromise<any> {
             //VARIABLES
-            let url = 'schools/';
+            let self = this;
+            let url = this.SCHOOL_URI;
+            let deferred = this.$q.defer();
 
-            return this.restApi.show({url: url, id: id}).$promise
+            this.restApi.show({url: url, id: id}).$promise
                 .then(
-                    function(data) {
-                        return data;
-                    }
-                ).catch(
-                    function(err) {
-                        console.log(err);
-                        return err;
+                    function(response) {
+                        deferred.resolve(response);
+                    },
+                    function(error) {
+                        DEBUG && console.error(error);
+                        if(error.statusText == 'Unauthorized') {
+                            self.AuthService.logout();
+                        }
+                        deferred.reject(error);
                     }
                 );
+
+            return deferred.promise;
         }
+
+
+
+        /**
+        * getSchoolByUserId
+        * @description - get school by user profile id filter value
+        * @function
+        * @return {angular.IPromise<any>} return a promise with user schools associated data
+        */
+        getSchoolByUserId(userId): angular.IPromise<any> {
+            //VARIABLES
+            let self = this;
+            let url = this.USER_SCHOOL_URI + userId;
+            let deferred = this.$q.defer();
+
+            this.restApi.queryObject({url: url}).$promise
+                .then(
+                    function(response) {
+                        if(response.results) {
+                            let res = response.results[0] ? response.results[0] : '';
+                            deferred.resolve(res);
+                        } else {
+                            DEBUG && console.error(response);
+                            deferred.reject(response);
+                        }
+                    },
+                    function(error) {
+                        DEBUG && console.error(error);
+                        if(error.statusText == 'Unauthorized') {
+                            self.AuthService.logout();
+                        }
+                        deferred.reject(error);
+                    }
+                );
+
+            return deferred.promise;
+        }
+
+
 
         /**
         * getAllSchools
@@ -83,20 +148,27 @@ module app.models.school {
         */
         getAllSchools(): angular.IPromise<any> {
             //VARIABLES
-            let url = 'schools/';
+            let self = this;
+            let url = this.SCHOOL_URI;
+            let deferred = this.$q.defer();
 
-            return this.restApi.query({url: url}).$promise
+            this.restApi.queryObject({url: url}).$promise
                 .then(
-                    function(data) {
-                        return data;
-                    }
-                ).catch(
-                    function(err) {
-                        console.log(err);
-                        return err;
+                    function(response) {
+                        deferred.resolve(response);
+                    },
+                    function(error) {
+                        DEBUG && console.error(error);
+                        if(error.statusText == 'Unauthorized') {
+                            self.AuthService.logout();
+                        }
+                        deferred.reject(error);
                     }
                 );
+
+            return deferred.promise;
         }
+
 
     }
 
