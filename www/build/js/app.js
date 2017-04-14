c65da4429da10653431180a0cdd451ae57ca804b
sergioruizdavila <sergioruizdavila@gmail.com>
2017-04-14 12:29:52 -0500

    Mod: Prepare to deploy on PRD environment

--- www/build/js/app.js
+++ www/build/js/app.js
@@ -97,13 +97,13 @@
 
 //# sourceMappingURL=../../maps/app/app.core.module.js.map
 
-DEBUG = true;
+DEBUG = false;
 (function () {
     'use strict';
     var BASE_URL = 'https://waysily-server-production.herokuapp.com/api/v1/';
     var BUCKETS3 = 'waysily-img/profile-avatar-prd';
     if (DEBUG) {
-        BASE_URL = 'https://waysily-server-staging.herokuapp.com/api/v1/';
+        BASE_URL = 'http://127.0.0.1:8000/api/v1/';
         BUCKETS3 = 'waysily-img/profile-avatar-dev';
     }
     var dataConfig = {
