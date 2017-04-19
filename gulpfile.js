d8cdc09533c42ac18b5ba23e6035d7af9f460c61
sergioruizdavila <sergioruizdavila@gmail.com>
2017-04-19 01:51:35 -0500

    Mod: refactor serverprod gulp task in order to redirect to https

--- gulpfile.js
+++ gulpfile.js
@@ -215,7 +215,7 @@ gulp.task('serveprod', function() {
       https: false,
       middleware: function(req, res, next) {
         if (req.headers['x-forwarded-proto'] != 'https') {
-            console.log('res: ', JSON.stringify(res));
+            console.log('res: ', res);
             res.redirect('302', 'https://' + req.hostname + req.originalUrl);
         }
         else {
