73de39bb2857d1353194cce656a3b3e5ee12e71a
sergioruizdavila <sergioruizdavila@gmail.com>
2017-04-22 17:07:26 -0500

    test: testing title and description text in order to validate SEO

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require("express");
+
+var app = express();
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
